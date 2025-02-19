d4451e56b725fe46bc54b336f710f452142ca0ca
Felipe <Lipenobremartiniano2016@gmail.com>
2025-02-19 15:03:04 -0300

    mais sobre os estudos de operadores em java

---
+++ tipos-variaveis/src/operadores_2.java
@@ -0,0 +1,12 @@
+public class operadores_2 {
+    public static void main(String [] args){
+    int numero = 5;
+
+    numero = - numero;
+    System.out.println(numero);
+
+    numero = numero * -1;
+
+    System.out.println(numero);
+ }
+}
---
+++ tipos-variaveis/src/operadores_3.java
@@ -0,0 +1,11 @@
+public class operadores_3 {
+    public static void main(String [] args){
+        int numero = 5;
+        
+
+        System.out.println(numero  ++);
+
+        System.out.println(numero);
+
+    }
+}
